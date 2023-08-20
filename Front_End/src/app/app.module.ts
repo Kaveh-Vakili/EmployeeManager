02b01c7f9cb7cf6c0c1cf0237abcd67a7e85cf9e
Kaveh Vakili <50510650+Kaveh-Vakili@users.noreply.github.com>
2023-08-19 20:02:05 -0400

    fixing emplyee service class

--- Front_End/src/app/app.module.ts
+++ Front_End/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [employeeservice],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
